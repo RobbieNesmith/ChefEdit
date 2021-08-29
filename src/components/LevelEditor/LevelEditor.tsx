3b6d88f216f4434ca52a69c4bd50c16e9456ca8d
Robbie Nesmith <bobberto1995@gmail.com>
2021-08-29 15:29:46 -0500

    Add drawable tile layers

--- src/components/LevelEditor/LevelEditor.tsx
+++ src/components/LevelEditor/LevelEditor.tsx
@@ -1,10 +1,49 @@
+import { useState } from "react";
+import useSelectedTiles from "../../hooks/useSelectedTiles";
 import "./LevelEditor.css";
 import TileGrid from "./TileGrid";
 
+function getBlankTileGrid(): Array<number> {
+    return Array(20 * 15).fill(0);
+}
+
 export default function LevelEditor() {
+    const [backgroundTiles, setBackgroundTiles] = useState(getBlankTileGrid());
+    const [foregroundTiles, setForegroundTiles] = useState(getBlankTileGrid());
+    const {leftClickTileId, rightClickTileId, setLeftClickTileId, setRightClickTileId} = useSelectedTiles();
+
+    function placeBackgroundTileAtIndex(index: number, button: number) {
+        const tileToAdd = button === 0 ? leftClickTileId : rightClickTileId;
+        setBackgroundTiles((bt) => {
+            return [...bt.slice(0, index), tileToAdd, ...bt.slice(index + 1)];
+        });
+    }
+
+    function placeForegroundTileAtIndex(index: number, button: number) {
+        const tileToAdd = button === 0 ? leftClickTileId : rightClickTileId;
+        setForegroundTiles((ft) => {
+            return [...ft.slice(0, index), tileToAdd, ...ft.slice(index + 1)];
+        });
+    }
+
+    function pickTile(tileId: number) {
+        setLeftClickTileId(tileId);
+    }
+
     return(
         <div className="LevelEditor">
-            <TileGrid />
+            <div className="TileGridHolder">
+                <TileGrid
+                    tiles={backgroundTiles}
+                    onTilePicked={pickTile}
+                    onTilePlaced={placeBackgroundTileAtIndex}
+                />
+                <TileGrid
+                    tiles={foregroundTiles}
+                    onTilePicked={pickTile}
+                    onTilePlaced={placeForegroundTileAtIndex}
+                />
+            </div>
         </div>
     );
 }
--- src/components/LevelEditor/TileGrid.tsx
+++ src/components/LevelEditor/TileGrid.tsx
@@ -1,3 +1,79 @@
-export default function TileGrid() {
-    return <div className="TileGrid"></div>;
+import React, { useState } from "react";
+
+function getOffsetForTileId(index: number) {
+    return {
+        xOffset: index % 16 * -32,
+        yOffset: Math.floor(index / 16) * -32
+    };
+}
+
+export default function TileGrid(props: {
+    tiles: Array<number>,
+    onTilePlaced(index: number, button: number):void,
+    onTilePicked(tileId: number): void
+}) {
+    const [leftMouseDown, setLeftMouseDown] = useState(false);
+    const [rightMouseDown, setRightMouseDown] = useState(false);
+
+    function handleMouseDown(index: number, evt: React.MouseEvent) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        if (evt.button === 0) {
+            setLeftMouseDown(true);
+            setRightMouseDown(false);
+            props.onTilePlaced(index, 0);
+        } else if (evt.button === 1) {
+            setLeftMouseDown(false);
+            setRightMouseDown(false);
+            props.onTilePicked(props.tiles[index]);
+        } else if (evt.button === 2) {
+            setLeftMouseDown(false);
+            setRightMouseDown(true);
+            props.onTilePlaced(index, 2);
+        }
+    }
+
+    function handleMouseMove(index: number, evt: React.MouseEvent) {
+        if (leftMouseDown) {
+            props.onTilePlaced(index, 0);
+        } else if (rightMouseDown) {
+            props.onTilePlaced(index, 2);
+        }
+        evt.stopPropagation();
+        evt.preventDefault();
+    }
+
+    function handleMouseUp(evt: React.MouseEvent) {
+        setLeftMouseDown(false);
+        setRightMouseDown(false);
+        evt.stopPropagation();
+        evt.preventDefault();
+    }
+
+    return (
+        <div
+            className="TileGrid"
+            onContextMenu={evt => {
+                evt.preventDefault();
+                evt.stopPropagation();
+            }}
+        >
+            {props.tiles.map((tileId, index) => {
+                const offset = getOffsetForTileId(tileId);
+                return (
+                    <div
+                        key={index}
+                        className="Tile" 
+                        style={{
+                            backgroundPositionX: offset.xOffset,
+                            backgroundPositionY: offset.yOffset
+                        }}
+                        onMouseDown={(evt) => handleMouseDown(index, evt)}
+                        onMouseMove={(evt) => handleMouseMove(index, evt)}
+                        onMouseUp={(evt) => handleMouseUp(evt)}
+                    />
+                );
+            })}
+        </div>
+    );
 }
