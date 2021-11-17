877b7e08a06ec647591674f6d58a81001bd74542
Robbie Nesmith <robbie@newbo.co>
2021-11-17 17:33:07 -0600

    Update MobInfo to new Mob model

--- src/components/MobSidebar/MobInfo.tsx
+++ src/components/MobSidebar/MobInfo.tsx
@@ -9,7 +9,8 @@ export default function MobInfo(props: { mob: Mob }) {
             <div>
                 {mob.startingCoordinates.x}, {mob.startingCoordinates.y}
             </div>
-            <div><input type="checkbox" disabled checked={!!mob.extraData} /> Has extra data?</div>
+            <div>Unknown1: {mob.unknown1}</div>
+            <div>Unknown2: {mob.unknown2}</div>
             <div><input type="checkbox" disabled checked={!!mob.flippedH} /> Flipped horizontally?</div>
             <div>Path Coordinates</div>
             <div>
